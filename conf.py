6acf5ab014ef275477c1b6586b87ffc65b4fd62c
H. Turgut Uyar <uyar@tekir.org>
2020-04-03 22:52:32 +0300

    site ana adresini https'e geçir

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "purespace"  # (translatable)
 BLOG_TITLE = "purespace.ist"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://purespace.ist/"
+SITE_URL = "https://purespace.ist/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
